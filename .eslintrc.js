a3974114306d66e50c092c24b0b1b41b9ac107ca
Felipe Lima <felipelima@ioasys.com.br>
2022-08-31 13:58:49 -0300

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,71 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks',
+    '@typescript-eslint',
+    'react-native',
+  ],
+  globals: {
+    __DEV__: true,
+    fetch: false,
+  },
+  env: {
+    jest: true,
+    es2020: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'jsx-quotes': 'off',
+    'comma-dangle': 'off',
+
+    'react/jsx-filename-extension': [
+      1,
+      {extensions: ['.js', '.jsx', '.tsx', '.ts']},
+    ],
+    'import/prefer-default-export': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/prop-types': 'off',
+    'react/state-in-constructor': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    strict: 0,
+    'import/no-named-as-default': 0,
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
     },
   ],
 };
