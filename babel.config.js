a77177ce0a30c54dd8cf9eac8075d1b521fc14cb
Felipe Lima <felipelima@ioasys.com.br>
2022-08-31 13:54:47 -0300

    feat configure paths mapping and dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@presentational': './src/presentational',
+          '@config': './src/config',
+          '@store': './src/store',
+          '@styles': './src/styles',
+          '@hooks': './src/hooks',
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@__mocks__': './__mocks__',
+          '@__tests__': './__tests__',
+        },
+      },
+    ],
+    ['module:react-native-dotenv'],
+  ],
 };
