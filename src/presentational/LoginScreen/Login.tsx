299f6643cf18a1afc18044ed997d7c320e3f2ea7
Felipe Lima <felipelima@ioasys.com.br>
2022-09-09 15:02:06 -0300

    feat home and login screen

--- src/presentational/LoginScreen/Login.tsx
+++ src/presentational/LoginScreen/Login.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { AppStackParamList } from '@routes/stacks/AppStack';
-
-import { useNavigation } from '@react-navigation/native';
-
-type NavProps = NativeStackNavigationProp<AppStackParamList, 'LoggedStack'>;
 
 export const Login = () => {
-  const { navigate } = useNavigation<NavProps>();
-
   return (
     <StyledContainer>
-      <StyledTitle>Login</StyledTitle>
-
-      <StyledButton
-        title="go to home"
-        onPress={() => navigate('LoggedStack')}
-      />
+      <StyledText>Login</StyledText>
     </StyledContainer>
   );
 };
 
 const StyledContainer = styled.View``;
 
-const StyledTitle = styled.Text``;
-
 const StyledButton = styled.Button``;
+
+const StyledText = styled.Text``;
