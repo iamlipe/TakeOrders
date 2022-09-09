754f1f2e2a1822853309efbaa18f44ceb4dba2a0
Felipe Lima <felipelima@ioasys.com.br>
2022-09-09 14:57:02 -0300

    feat config all models

---
+++ src/database/models/orderModel.ts
@@ -0,0 +1,18 @@
+import { Model } from '@nozbe/watermelondb';
+import { field, relation } from '@nozbe/watermelondb/decorators';
+import { Bill } from './billModel';
+import { Product } from './productModel';
+
+export class Order extends Model {
+  static table = 'orders';
+
+  @field('productId') productId!: string;
+
+  @field('billId') billId!: string;
+
+  @field('quantity') quantity!: number;
+
+  @relation('bills', 'billId') bill!: Bill;
+
+  @relation('products', 'productId') product!: Product;
+}
